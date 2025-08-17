e4ce066d3302d1a33e390ecb292493b6b7dda56a
Viktoriia <struk.victoria@gmail.com>
2025-08-17 23:04:48 +0300

    Add host

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 
 const PORT = process.env.PORT;
-const HOST = process.env.HOST || '127.0.0.1';
+const HOST = process.env.HOST || '0.0.0.0';
 
 const httpServer = http.createServer(app);
 
