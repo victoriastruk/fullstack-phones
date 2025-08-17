f739706aafd08cd462da077df91f372a46e8feaf
Viktoriia <struk.victoria@gmail.com>
2025-08-17 23:21:29 +0300

    Add server url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({ baseURL: "http://localhost:5000/api" });
+const axiosInstance = axios.create({ baseURL: import.meta.env.VITE_API_URL + "/api"});
 
 export const getCpus = () => axiosInstance.get("/cpus");
 
