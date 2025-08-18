686e77e0139ebb7f4501212b70caf01248ad88be
Viktoriia <struk.victoria@gmail.com>
2025-08-18 15:20:56 +0300

    Add production data

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,11 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
     seederStorage: 'sequelize',
     dialectOptions: {
