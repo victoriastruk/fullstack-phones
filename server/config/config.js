27163dea17cdd5178753bfadc061e170bf3714a7
Viktoriia <struk.victoria@gmail.com>
2025-08-22 18:47:02 +0300

    Change config

--- server/config/config.js
+++ server/config/config.js
@@ -15,23 +15,9 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  },
-  migration: {
-    use_env_variable: 'DIRECT_URL',
-    dialect: 'postgres',
+    use_env_variable: "DATABASE_URL", 
+    dialect: "postgres",
+    seederStorage: "sequelize",
     dialectOptions: {
       ssl: {
         require: true,
--- server/index.js
+++ server/index.js
@@ -6,6 +6,15 @@ const HOST = process.env.HOST || '0.0.0.0';
 
 const httpServer = http.createServer(app);
 
+app.get("/", async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    res.send("✅ Database connected successfully");
+  } catch (error) {
+    res.status(500).send("❌ Database Error: " + error.message);
+  }
+});
+
 httpServer.listen(PORT, HOST, () =>
   console.log(`Server is listening http://${HOST}:${PORT}`)
 );
